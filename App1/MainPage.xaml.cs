fc6bd1f971ab951450940612aebd7564c7acb806
JORGE ARMANDO JUAREZ VAZQUEZ <2018371036@uteq.edu.mx>
2021-09-14 22:18:19 -0500

    First push
    This is the first push

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        int _count;
+        string _message;
+        Button button1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _count = 0;
+        }
+
+        private void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            if(button1 == null) 
+                button1 = ((Button)sender);
+            _count++;
+            _message = "You clicked me {0} times";
+            button1.Text = string.Format(_message, _count);
+        }
+
+        private void Reset_Clicked(object sender, System.EventArgs e)
+        {
+            _message = "Click me";
+            if (button1 != null)
+                button1.Text = _message;
+            _count = 0;
+        }
+    }
+}
